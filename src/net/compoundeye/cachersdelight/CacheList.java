7e56b6ff46b37fce345f90179cf8052e3ad841c8
andre.wichmann.de <andre.wichmann.de@4cac83e8-9d2c-11de-bca0-7bf21e48512a>
2009-09-19 08:22:02 +0000

    - added missing mystery cache gfx - added GlobalConstants (with type and size defs) - added GeocacheData - added GeocacheOverviewData - added GeocacheDetailData

--- src/net/compoundeye/cachersdelight/CacheList.java
+++ src/net/compoundeye/cachersdelight/CacheList.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class CacheList extends Activity {
+
+	// Log tag
+	private static final String TAG = "CacheList";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
---
+++ src/net/compoundeye/cachersdelight/GeocacheOverviewData.java
@@ -0,0 +1,10 @@
+package net.compoundeye.cachersdelight;
+
+public class GeocacheOverviewData {
+
+	public String id, name, owner;
+	// Coordinates saved as int for speed
+	public int longDegree, longFrac, latDegree, latFrac;
+	public int terrain, difficulty, rating;
+	
+}
