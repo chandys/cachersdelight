9d37475547145d668ca0888cc3bb2abd73ca58bf
andre.wichmann.de <andre.wichmann.de@4cac83e8-9d2c-11de-bca0-7bf21e48512a>
2009-09-19 08:28:06 +0000

    - added missing cachetype webcam

--- trunk/src/net/compoundeye/cachersdelight/GeocacheOverviewData.java
+++ trunk/src/net/compoundeye/cachersdelight/GeocacheOverviewData.java
@@ -3,8 +3,7 @@ package net.compoundeye.cachersdelight;
 public class GeocacheOverviewData {
 
 	public String id, name, owner;
-	// Coordinates saved as int for speed
+	// Coordinates saved as int for performance
 	public int longDegree, longFrac, latDegree, latFrac;
 	public int terrain, difficulty, rating;
-	
 }
