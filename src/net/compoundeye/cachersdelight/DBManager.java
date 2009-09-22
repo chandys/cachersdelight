0bd6dfa3e8cf251bc093081f21a15b1a5cb854a8
andre.wichmann.de <andre.wichmann.de@4cac83e8-9d2c-11de-bca0-7bf21e48512a>
2009-09-22 18:57:14 +0000

    - moved cachetype and -size constants to GeocacheData - deleted silly interface GlobalConstants - filled in more methods in DBManager

--- src/net/compoundeye/cachersdelight/DBManager.java
+++ src/net/compoundeye/cachersdelight/DBManager.java
@@ -1,28 +1,32 @@
 package net.compoundeye.cachersdelight;
 
-import junit.framework.Assert;
 import android.content.*;
 import android.database.*;
 import android.database.sqlite.*;
-import android.util.*;
 
 
+/**
+ * Wrapper for access to geocache databases. Hides internal DB structure and
+ * exposes standard access methods.
+ * 
+ * @author Andre Wichmann
+ *
+ */
 public class DBManager {
 
 	private static final String TAG = "DBManager";
-	private DatabaseHelper mDbHelper;
-	private SQLiteDatabase mDb;
 
+	// SQL strings
 	private static final String DATABASE_CREATE =
 		"CREATE TABLE geocache_overview (" +
 			"_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-			"id TEXT NON NULL," +
+			"cacheid TEXT NON NULL," +
 			"name TEXT NON NULL" +
 			"owner TEXT NON NULL" +
 			"longitude REAL NON NULL" +
 			"latitude REAL NON NULL" +
-			"terrain INTEGER NON NULL" +
-			"difficulty INTEGER NON NULL" +
+			"terrain REAL NON NULL" +
+			"difficulty REAL NON NULL" +
 			"rating REAL NON NULL" +
 			");";
 
@@ -30,19 +34,67 @@ public class DBManager {
 	private static final String DATABASE_TABLE = "geocache_overview";
 	private static final int DATABASE_VERSION = 1;
 
-    private static class DatabaseHelper extends SQLiteOpenHelper {
+	
+	
+	private DatabaseHelper mDbHelper;
+	private SQLiteDatabase mDb;
+	private final Context mCtx;
 
-    	DatabaseHelper(Context context) {
-    		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    	}
+	/**
+	 * Inner helper class.
+	 * @see SQLiteOpenHelper
+	 */
+	private static class DatabaseHelper extends SQLiteOpenHelper {
 
-    	@Override
-    	public void onCreate(SQLiteDatabase db) {
-    		db.execSQL(DATABASE_CREATE);
-    	}
+		DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
 
-    	@Override
-    	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    	}
-    }
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(DATABASE_CREATE);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		}
+	}
+    
+	
+	
+	/**
+	 * Constructor. Needs the context to work.
+	 * 
+	 * @param ctx
+	 *            the Context within which to work
+	 */
+	public DBManager(Context ctx) {
+		this.mCtx = ctx;
+	}
+    
+
+	
+	/**
+	 * Opens the database. If none exists for the given name, a new
+	 * one is created. If that fails, an SQLException is thrown.
+	 * 
+	 * @return this (self reference, allowing this to be chained in an
+	 *         initialization call)
+	 * @throws SQLException
+	 *             if the database could be neither opened or created
+	 */
+	public DBManager open() throws SQLException {
+		mDbHelper = new DatabaseHelper(mCtx);
+		mDb = mDbHelper.getWritableDatabase();
+		return this;
+	}
+
+	
+	
+	/**
+	 * Closes the database.
+	 */
+	public void close() {
+		mDbHelper.close();
+	}
 }
